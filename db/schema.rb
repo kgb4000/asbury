6880f3a47b3b1560013dfb934a556ef40628ec8c
KGB4000 <kester4000@gmail.com>
2015-01-03 16:13:24 -0500

    Added paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150103163256) do
+ActiveRecord::Schema.define(version: 20150103210657) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -19,9 +19,13 @@ ActiveRecord::Schema.define(version: 20150103163256) do
     t.string   "time"
     t.text     "description"
     t.string   "image"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "location"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
