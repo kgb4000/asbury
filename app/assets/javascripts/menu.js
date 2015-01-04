86853d49676cacebe04e0c21027f73f4df7e7e34
KGB4000 <kester4000@gmail.com>
2015-01-04 03:25:44 -0500

    Added some more CSS to fix navigation

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.menu-text').click(function() {
+    $('.overlay').show('slow');
+      })
+      $('.menu-text-close').click(function() {
+        $('.overlay').hide('slow');
+      });
+});
